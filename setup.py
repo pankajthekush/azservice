a4c2248a5b4dc238cc09da52e0225f1b9c8eefd2
pankajthekush <pankajthekush@gmail.com>
2020-01-24 15:14:31 +0530

    Updated v0.1

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='supload',
+    version='0.1',
+    packages=['supload'],
+    install_requires=['boto3>=1.10.46']
+)
---
+++ supload/supload.py
@@ -0,0 +1,40 @@
+
+import boto3
+import json
+import os
+from botocore.exceptions import ClientError
+import sys
+import time
+
+
+def return_session(bucket_name,credential_json_file = 'keys.json'):
+    accesskey,secretkey = None,None
+    with open(credential_json_file) as f:
+        creds = json.load(f)
+        accesskey,secretkey = creds['acces_key'],creds['secret_access_key']
+
+    session = boto3.Session(aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    return bucket
+
+
+
+def upload_file(bucket_name,file_name,in_sub_folder):
+    fname = os.path.basename(file_name)
+    str_dt = time.strftime("%m%d%Y")
+    print(str_dt)
+    key = f'{in_sub_folder}/{str_dt}/{fname}'
+    already_exists = list(bucket_name.objects.filter(Prefix=key)).__len__() > 0
+    
+    if not already_exists:
+        print(f'Uploaindg : {key}...')
+        bucket_name.upload_file(fname,Key=key)
+    else:
+        print(f'File {key} already exits Skipping')
+
+
+if __name__ == "__main__":
+    bckt = return_session('kapowautostorerhoaiindia')
+    upload_file(bckt,'setup.py','LinkedIn/Profiles')
+    
