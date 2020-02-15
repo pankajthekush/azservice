31e4f435f54465c0323b89a34aad05e5c344acf1
pankajthekush <pankajthekush@gmail.com>
2020-02-15 16:56:43 +0530

    Updated whole repo to include amazon services

--- supload/supload.py
+++ supload/supload.py
@@ -7,47 +7,28 @@ import sys
 import time
 
 
-def return_session(bucket_name,credential_json_file = 'keys.json'):
-    accesskey,secretkey = None,None
-    with open(credential_json_file) as f:
-        creds = json.load(f)
-        accesskey,secretkey = creds['acces_key'],creds['secret_access_key']
-
-    session = boto3.Session(aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
-    s3 = session.resource('s3')
-    bucket = s3.Bucket(bucket_name)
-    return bucket
-
-
-
-def upload_file(bucket_name,file_name,in_sub_folder):
-    fname = os.path.basename(file_name)
-    str_dt = time.strftime("%m%d%Y")
-
-    key = f'{in_sub_folder}/{str_dt}/{fname}'
-    already_exists = list(bucket_name.objects.filter(Prefix=key)).__len__() > 0
-    
-    if not already_exists:
-        print(f'Uploaindg : {key}...')
-        bucket_name.upload_file(fname,Key=key)
-    else:
-        print(f'File {key} already exits Skipping')
-
-
-def is_already_exist(bucket_name,file_name,in_sub_folder):
+def upload_file(file_name,in_sub_folder,bucket_name):
+    client = boto3.client('s3')
     fname = os.path.basename(file_name)
     str_dt = time.strftime("%m%d%Y")
     key = f'{in_sub_folder}/{str_dt}/{fname}'
-    already_exists = list(bucket_name.objects.filter(Prefix=key)).__len__() > 0
+    client.upload_file(fname, Bucket=bucket_name ,Key=key)
     
-    if already_exists:
-        return True
-    else:
-        return False
 
+def is_already_exist(file_name,in_sub_folder,bucket_name=):
+    fname = os.path.basename(file_name)
+    str_dt = time.strftime("%m%d%Y")
+    key = f'{in_sub_folder}/{str_dt}/{fname}'
+
+    client = boto3.client('s3')
+
+    try:
+        client.head_object(Bucket=bucket_name,Key=key)
+    except ClientError as e:
+        return int(e.response['Error']['Code']) != 404
+    return True
 
 
 if __name__ == "__main__":
-    bckt = return_session('kapowautostorerhoaiindia')
-    print (is_already_exist(bckt,'setup.py','LinkedIn/Profiles'))
-    
+    pass
+ 
