f3b7905f70ab4db04d001edfcaf7c9f828e30c6c
pankajthekush <pankajthekush@gmail.com>
2020-02-17 13:12:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='azservice',
     version='0.3',
     packages=['supload','awins','azwmail'],
-    install_requires=['boto3>=1.10.46','keyring+=21.1.0'],
+    install_requires=['boto3>=1.10.46','keyring>=21.1.0'],
     entry_points ={'console_scripts': ['azrdp = awins.awins:connect_rdp',
                                         'azip = awins.awins:get_public_ip',
                                         'azstatus=awins.awins:get_instance_status',
