348ec6fb32bca52075329af216c9f4df00a6a3e8
pankajthekush <pankajthekush@gmail.com>
2020-02-17 10:47:27 +0530

    Updated entry pointsfor awis

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ setup(
     name='supload',
     version='0.2',
     packages=['supload','awins'],
-    install_requires=['boto3>=1.10.46']
+    install_requires=['boto3>=1.10.46'],
+    entry_points ={'console_scripts': ['azrdp = awins.awins:get_instance_id',
+                                        'azip = awins.awins:get_public_ip',
+                                        'azstatus=awins.awins:get_instance_status',
+                                        'azstop=awins.awins:stop_instance',
+                                        'azstart=awins.awins:start_instance'
+                                        ]}
+
 )
